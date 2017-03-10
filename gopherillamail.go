7b8b2517bc10c5bce4facc54edefe3c0f65f27bd
Maurits van Altvorst <mvanaltvorst@icloud.com>
2017-03-10 19:37:13 +0100

    Add Mail struct

--- gopherillamail.go
+++ gopherillamail.go
@@ -2,7 +2,19 @@ package gopherillamail
 
 import (
     "fmt"
+    "net/http"
+    "time"
 )
+
+type Mail struct {
+    guid string
+    subject string
+    sender string
+    time time.Time
+    read bool
+    excerpt string
+    body string
+}
 
 func main() {
 
